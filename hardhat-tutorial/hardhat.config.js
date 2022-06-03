77c4251a633aed7807a2fdc60b3e722001e7b482
pa-ak <pa-ak@users.noreply.github.com>
2022-06-03 13:55:49 +0400

    initial commit

---
+++ hardhat-tutorial/hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config({ path: ".env" });
+
+const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
+
+const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
+
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: ALCHEMY_API_KEY_URL,
+      accounts: [RINKEBY_PRIVATE_KEY],
+    },
+  },
+};
+
+// 0xC2EC3f60F53CA73e482e326d642CBCE52F2fC839 - deployed CryptoDevs contract address
